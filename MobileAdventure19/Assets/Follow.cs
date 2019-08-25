22e4e212cfe149b1cae3adcf4ed3d01a74feaa7d
anthuny <41554752+anthuny@users.noreply.github.com>
2019-08-26 03:44:39 +1000

    Added player movement, camera follow

---
+++ MobileAdventure19/Assets/Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+    private GameObject target;
+
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+    }
+}
---
+++ MobileAdventure19/Assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    Vector2 playerPos;
+    public float playerSpeed = .2f;
+
+    private void Start()
+    {
+        playerPos = transform.position;
+        
+    }
+    void FixedUpdate()
+    {
+        //Call function(s) every frame
+        PlayerMovement();
+
+        transform.position = playerPos;
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerPos.x -= playerSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerPos.x += playerSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerPos.y += playerSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerPos.y -= playerSpeed;
+        }
+    }
+}
