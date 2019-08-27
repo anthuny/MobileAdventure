9edda3c3022290e58e46024cf3c7e4676dfb40ff
anthuny <41554752+anthuny@users.noreply.github.com>
2019-08-27 22:14:42 +1000

    Added truths and lies, added score

--- MobileAdventure19/Assets/Gamemode.cs
+++ MobileAdventure19/Assets/Gamemode.cs
@@ -1,24 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gamemode : MonoBehaviour
 {
-    int wrongAns;
+    public int wrongAns;
+    public bool winCondition;
+    public int score = 0;
+
+    public Text scoreText;
+    public Text endGameText;
     // Start is called before the first frame update
     void Start()
     {
         Think();
+        score = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Think()
     {
+        //Set direction
+        wrongAns = Random.Range(1, 5);
+        
+        //Set text to display direction
+        if (wrongAns == 1)
+        {
+            endGameText.text = "Go Left";
+        }
+
+        if (wrongAns == 2)
+        {
+            endGameText.text = "Go Up";
+        }
+
+        if (wrongAns == 3)
+        {
+            endGameText.text = "Go Right";
+        }
+
+        if (wrongAns == 4)
+        {
+            endGameText.text = "Go Down";
+        }
+    }
+
+    public void SetRoundEnd()
+    {
+        //If the player chooses incorrectly
+        if (!winCondition)
+        {
+            endGameText.text = "Wrong!".ToString();
+
+            //Call function
+            ScoreReset();
+        }
+        
+        //If the player chooses correctly
+        else
+        {
+            //Call function
+            ScoreUp();
+        }
+    }
+
+    //Increase the score by 1
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
         
     }
 
-    void Think()
+    //Reset the score
+    void ScoreReset()
     {
-        wrongAns = Random.Range(1, 4);
+        score = 0;
+        scoreText.text = score.ToString();
     }
 }
--- MobileAdventure19/Assets/Player.cs
+++ MobileAdventure19/Assets/Player.cs
@@ -10,15 +10,17 @@ public class Player : MonoBehaviour
 
     public float playerSpeed = .2f;
 
-
     private void Start()
     {
-        
+        //Find components
         rb = GetComponent<Rigidbody2D>();
-        
+
+
+
     }
     void FixedUpdate()
     {
+        //Player Movement
         playerPos = Vector3.zero;
         playerPos.x = Input.GetAxisRaw("Horizontal");
         playerPos.y = Input.GetAxisRaw("Vertical"); 
@@ -30,16 +32,138 @@ public class Player : MonoBehaviour
 
     void PlayerMovement()
     {
+        //The functionality behind the movement
         rb.MovePosition(transform.position + playerPos * playerSpeed * Time.deltaTime);
     }
 
+    //Upon colliding with object called 'collider' reset position
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "Collider")
+        //Find gamemode script
+        GameObject gamemode = GameObject.Find("Gamemode");
+        Gamemode gamemodeScript = gamemode.GetComponent<Gamemode>();
+
+                                                                                                //The four options for getting the answer correct
+        //Going East
+        if (other.gameObject.name == "Collider East" && gamemodeScript.wrongAns != 1)
         {
+            //Reset player's powision to middle of area
             transform.position = new Vector2(0, 5);
-            
+
+            //calling coroutine
+            StartCoroutine("SetWin");
+            return;
         }
 
+        //Going North
+        if (other.gameObject.name == "Collider North" && gamemodeScript.wrongAns != 2)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetWin");
+            return;
+        }
+
+        //Going West
+        if (other.gameObject.name == "Collider West" && gamemodeScript.wrongAns != 3)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetWin");
+            return;
+        }
+
+        //Going South
+        if (other.gameObject.name == "Collider South" && gamemodeScript.wrongAns != 4)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetWin");
+            return;
+        }
+
+                                                                                             //The four options for getting the answer Wrong
+        //Going East
+        if (other.gameObject.name == "Collider East" && gamemodeScript.wrongAns == 1)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetLose");
+            return;
+        }
+
+        //Going North
+        if (other.gameObject.name == "Collider North" && gamemodeScript.wrongAns == 2)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetLose");
+            return;
+
+        }
+
+        //Going West
+        if (other.gameObject.name == "Collider West" && gamemodeScript.wrongAns == 3)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetLose");
+            return;
+        }
+
+        //Going South
+        if (other.gameObject.name == "Collider South" && gamemodeScript.wrongAns == 4)
+        {
+            //Reset player's powision to middle of area
+            transform.position = new Vector2(0, 5);
+
+            //calling coroutine
+            StartCoroutine("SetLose");
+            return;
+        }
+    }
+
+    IEnumerator SetWin()
+    {
+        //Find gamemode script
+        GameObject gamemode = GameObject.Find("Gamemode");
+        Gamemode gamemodeScript = gamemode.GetComponent<Gamemode>();
+
+        //Sets screen text
+        gamemodeScript.winCondition = true;
+        gamemodeScript.SetRoundEnd();
+
+        yield return new WaitForSeconds(0.25f);
+
+        //Enemy thinks of new wrong answer
+        gamemodeScript.Think();
+    }
+
+    IEnumerator SetLose()
+    {
+        //Find gamemode script
+        GameObject gamemode = GameObject.Find("Gamemode");
+        Gamemode gamemodeScript = gamemode.GetComponent<Gamemode>();
+
+        //Sets screen text
+        gamemodeScript.winCondition = false;
+        gamemodeScript.SetRoundEnd();
+
+        yield return new WaitForSeconds(0.25f);
+
+        //Enemy thinks of new wrong answer
+        gamemodeScript.Think();
     }
 }
